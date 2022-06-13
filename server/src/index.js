0ec8efd74bbf8b66ab539149b6cc977444909c3f
jae <goawmfhfl1@naver.com>
2022-06-14 07:14:47 +0900

    Add: Schema

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,9 @@
+const { ApolloServer }= require('apollo-server');
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({typeDefs})
+
+server.listen().then(()=>{
+    console.log(`server is running`);
+})
 //TODO
