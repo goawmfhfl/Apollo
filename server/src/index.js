3c7eee62b9e8346272dd31030ee8f1e747cabf8e
jaeyoung <jy.choi@chopsticks.market>
2022-06-14 10:28:54 +0900

    Add: MockUp Data

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,29 @@
-const { ApolloServer }= require('apollo-server');
-const typeDefs = require('./schema');
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
 
-const server = new ApolloServer({typeDefs})
+const mocks = {
+  Query: () => ({
+    tracksForHome: () => [...new Array(9)],
+  }),
+  Track: () => ({
+    id: () => "track_01",
+    title: () => "Astro Kitty, Space Explorer",
+    author: () => {
+      return {
+        name: "Grumpy Cat",
+        photo:
+          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+      };
+    },
+    thumbnail: () =>
+      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+    length: () => 1210,
+    modulesCount: () => 6,
+  }),
+};
 
-server.listen().then(()=>{
-    console.log(`server is running`);
-})
-//TODO
+const server = new ApolloServer({ typeDefs, mocks: true });
+
+server.listen().then(() => {
+  console.log(`서버가 달립니다 !! server is running`);
+});
